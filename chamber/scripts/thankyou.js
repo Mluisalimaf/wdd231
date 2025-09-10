6d39477d754d3bccb7aec066125b0650dda70046
Maria Luisa Ferraz <140729288+Mluisalimaf@users.noreply.github.com>
2025-09-09 21:21:43 -0300

    Delete chamber directory

--- chamber/scripts/thankyou.js
+++
@@ -1,8 +0,0 @@
-// Retrieve form data from URL parameters
-const urlParams = new URLSearchParams(window.location.search);
-document.getElementById('firstName').textContent = urlParams.get('first');
-document.getElementById('lastName').textContent = urlParams.get('last');
-document.getElementById('email').textContent = urlParams.get('email');
-document.getElementById('phone').textContent = urlParams.get('phone');
-document.getElementById('organization').textContent = urlParams.get('organization');
-document.getElementById('timestamp').textContent = urlParams.get('timestamp');
