1166f4a4e0a9e19a11130aef25053fd85a7331c0
maria luisa <limamarialuisa06@gmail.com>
2025-10-14 02:15:54 -0300

    finalized?

---
+++ finalproject/scripts/thankyou.js
@@ -0,0 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search);
+document.getElementById('firstName').textContent = urlParams.get('first');
+document.getElementById('lastName').textContent = urlParams.get('last');
+document.getElementById('email').textContent = urlParams.get('email');
+document.getElementById('phone').textContent = urlParams.get('phone');
+document.getElementById('dateSubmitted').textContent = urlParams.get('dateSubmitted');
+document.getElementById('timeSubmitted').textContent = urlParams.get('timeSubmitted');
